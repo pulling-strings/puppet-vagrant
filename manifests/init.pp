5974c3d683066734f54412bc0a9e75b71a9e1b68
narkisr <narkisr@gmail.com>
2014-01-02 16:25:45 +0200

    supporting 1.4.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,14 @@
 # a module which install vagrant, since it lacks PPA its akward
 class vagrant {
-  $url  = 'http://files.vagrantup.com/packages'
-  $hash = 'a40522f5fabccb9ddabad03d836e120ff5d14093'
-  $deb  = 'vagrant_1.3.5_x86_64.deb'
+  $url  = 'http://dl.bintray.com/mitchellh/vagrant/'
+  $version = '1.4.2'
+  $deb  = "vagrant_${version}_x86_64.deb"
 
   exec{'download vagrant deb':
-    command => "wget -P /tmp ${url}/${hash}/${deb}",
+    command => "wget -P /tmp ${url}/${deb}",
     user    => 'root',
     path    => ['/usr/bin','/bin'],
-    unless  => 'which vagrant'
+    unless  => "which vagrant && /usr/bin/vagrant -v | /bin/grep ${version}"
   } ->
 
   package{'vagrant':
