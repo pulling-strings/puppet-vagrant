37891470a67d1c257fab9daccee40c6972565d23
ronen <narkisr@gmail.com>
2014-12-15 23:33:00 +0200

    fixing HOME issue (causing unless to fail) and moving to 1.7.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,26 @@
 # a module which install vagrant, since it lacks PPA its akward
-class vagrant {
+class vagrant($user='') {
   include vagrant::pecrant
   include vagrant::nfs
 
   $url  = 'https://dl.bintray.com/mitchellh/vagrant/'
-  $version = '1.6.5'
+  $version = '1.7.1'
 
   $deb  = "vagrant_${version}_x86_64.deb"
 
   exec{'download vagrant deb':
-    command => "wget -P /tmp ${url}/${deb}",
-    user    => 'root',
-    path    => ['/usr/bin','/bin'],
-    unless  => "which vagrant && /usr/bin/vagrant -v | /bin/grep ${version}"
-  } ->
+    command     => "wget -P /tmp ${url}/${deb}",
+    environment => ['HOME=/home/ronen/'],
+    user        => 'root',
+    path        => ['/usr/bin','/bin'],
+    unless      => "test -f /usr/bin/vagrant && (/usr/bin/vagrant -v | /bin/grep ${version})"
+  } ~>
 
   exec{'install vagrant deb':
-    command => "dpkg -i /tmp/${deb}",
-    user    => 'root',
-    path    => ['/usr/bin','/bin','/usr/local/sbin', '/usr/sbin', '/sbin'],
-    unless  => "which vagrant && /usr/bin/vagrant -v | /bin/grep ${version}"
+    command      => "dpkg -i /tmp/${deb}",
+    refreshonly => true,
+    user         => 'root',
+    path         => ['/usr/bin','/bin','/usr/local/sbin', '/usr/sbin', '/sbin'],
   }
 
 }
