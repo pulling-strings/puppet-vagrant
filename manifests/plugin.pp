56c230232b1c5925cde1ba480aca7d0f2887a6dc
ronen <narkisr@gmail.com>
2015-02-03 14:03:47 +0200

    lint fix

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,22 +1,23 @@
+# Installing a vagrant plugin
 define vagrant::plugin(
   $user,
   $home,
   $version=undef
-  ) {
-  $command_base = "vagrant plugin install $name"
+) {
+  $command_base = "vagrant plugin install ${name}"
   if $version != undef {
     $command = "${command_base} --plugin-version ${version}"
   } else {
     $command = $command_base
   }
 
-  exec{"vagrant-plugin-install-$name":
-    command => $command,
-    path => "/opt/vagrant/bin:/usr/local/bin:/usr/bin:/bin",
-    unless => "vagrant plugin list |grep -q $name",
-    user => $user,
-    provider => shell,
-    environment => ["HOME=$home"],
-    require => Package[vagrant]
+  exec{"vagrant-plugin-install-${name}":
+    command     => $command,
+    path        => '/opt/vagrant/bin:/usr/local/bin:/usr/bin:/bin',
+    unless      => "vagrant plugin list | grep -q ${name}",
+    user        => $user,
+    provider    => shell,
+    environment => ["HOME=${home}"],
+    require     => Package[vagrant]
   }
 }
