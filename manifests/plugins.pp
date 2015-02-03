642048a3e90e926caba69ec2b29a43a0f75af897
ronen <narkisr@gmail.com>
2015-02-03 14:03:07 +0200

    using vagrant home

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,14 +1,13 @@
 # Setting up vagrant plugins
 class vagrant::plugins(
   $plugins=[],
-  $home,
   $user
 ) {
 
   if($plugins!=[]){
     vagrant::plugin{$plugins:
       user => $user,
-      home => $home
+      home => $::vagrant::home
     }
   }
 }
