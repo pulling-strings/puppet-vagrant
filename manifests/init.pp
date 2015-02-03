ccabaec442b105a3533c02a1292d54da98e4f40e
ronen <narkisr@gmail.com>
2015-02-03 14:03:37 +0200

    including plugins and tidying up

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # a module which install vagrant, since it lacks PPA its akward
-class vagrant() {
+class vagrant($home) {
   include vagrant::nfs
+  include vagrant::plugins
 
   $url  = 'https://dl.bintray.com/mitchellh/vagrant/'
   $version = '1.7.2'
@@ -9,14 +10,15 @@ class vagrant() {
 
   exec{'download vagrant deb':
     command     => "wget -P /tmp ${url}/${deb}",
-    environment => ['HOME=/home/ronen/'],
+    environment => ["HOME=${home}"],
     user        => 'root',
     path        => ['/usr/bin','/bin'],
-    unless      => "test -f /usr/bin/vagrant && (/usr/bin/vagrant -v | /bin/grep ${version})" } ~>
+    unless      => "test -f /usr/bin/vagrant && (/usr/bin/vagrant -v | /bin/grep ${version})"
+  } ~>
 
-  package{"vagrant":
-    source     => "/tmp/${deb}",
-    provider   => dpkg
+  package{'vagrant':
+    source   => "/tmp/${deb}",
+    provider => dpkg
   }
 
 }
