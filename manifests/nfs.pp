fd54e2ef59a052c452851611ac6bb79f356e70a3
ronen <narkisr@gmail.com>
2016-06-04 00:58:01 +0300

    enabling sudo only on prod env

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -1,13 +1,15 @@
 # Managing vagrant NFS integration
 class vagrant::nfs {
- 
-  # enabling passwordless nfs access
-  file { '/etc/sudoers.d/vagrant':
-    ensure=> file,
-    mode  => '0644',
-    source=> 'puppet:///modules/vagrant/vagrant-sudoer',
-    owner => root,
-    group => root,
+
+  if $::environment == 'prod' {
+    # enabling passwordless nfs access
+    file { '/etc/sudoers.d/vagrant':
+      ensure => file,
+      mode   => '0644',
+      source => 'puppet:///modules/vagrant/vagrant-sudoer',
+      owner  => root,
+      group  => root,
+    }
   }
 
   package{'nfs-kernel-server':
