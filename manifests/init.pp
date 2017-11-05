1cb9c3ce0a41c2478db636ffb0e232b4ab3761b3
ronen <narkisr@gmail.com>
2017-11-05 21:53:41 +1100

    version up

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,27 +6,30 @@ class vagrant(
   include vagrant::nfs
   include vagrant::plugins
   include vagrant::environment
+  include downloadfile
 
   validate_string($home, $user)
-  $version = '1.9.2'
+  $version = '2.0.0'
 
   $url = "https://releases.hashicorp.com/vagrant/${version}"
 
   $deb  = "vagrant_${version}_x86_64.deb"
 
-  exec{'download vagrant deb':
-    command     => "wget -P /usr/src/ ${url}/${deb}",
-    environment => ["HOME=${home}"],
-    user        => 'root',
-    path        => ['/usr/bin','/bin'],
-    timeout     => '1200',
-    unless      => "test -f /usr/src/${deb}"
-  } ->
+
+  downloadfile::and_md5check {'download vagrant deb':
+      url    => "${url}/${deb}",
+      dest   => "/usr/src/${deb}",
+      md5sum => '39131b8986b2fdcf44776d60fd125948',
+      user   => $::id,
+      group  => $::gid,
+      chmod  => '770'
+  }
 
   package{'vagrant':
     ensure   => latest,
     source   => "/usr/src/${deb}",
-    provider => dpkg
+    provider => dpkg,
+    require  => Downloadfile::And_md5check['download vagrant deb']
   }
 
 }
