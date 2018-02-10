3d804559cf043db5ae8389517923b1688a2d914f
ronen <narkisr@gmail.com>
2018-02-10 22:30:30 +1100

    latest vagrant

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class vagrant(
   include downloadfile
 
   validate_string($home, $user)
-  $version = '2.0.1'
+  $version = '2.0.2'
 
   $url = "https://releases.hashicorp.com/vagrant/${version}"
 
@@ -19,7 +19,7 @@ class vagrant(
   downloadfile::and_md5check {'download vagrant deb':
       url    => "${url}/${deb}",
       dest   => "/usr/src/${deb}",
-      md5sum => '6b5eb1f84e8c42e984c9b08800a4cade',
+      md5sum => '41f688a6ebe314bd67bc0968838e68fe',
       user   => $::id,
       group  => $::gid,
       chmod  => '770'
