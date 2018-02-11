5031304b3d3a69e35efcf1b68f01c0460ab859de
ronen <narkisr@gmail.com>
2018-02-11 20:56:52 +1100

    removing env

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -1,15 +1,13 @@
 # Managing vagrant NFS integration
 class vagrant::nfs {
 
-  if $::environment == 'prod' {
-    # enabling passwordless nfs access
-    file { '/etc/sudoers.d/vagrant':
-      ensure => file,
-      mode   => '0644',
-      source => 'puppet:///modules/vagrant/vagrant-sudoer',
-      owner  => root,
-      group  => root,
-    }
+  # enabling passwordless nfs access
+  file { '/etc/sudoers.d/vagrant':
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/vagrant/vagrant-sudoer',
+    owner  => root,
+    group  => root,
   }
 
   package{'nfs-kernel-server':
